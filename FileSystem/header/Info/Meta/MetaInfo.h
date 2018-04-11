4205953ba9adb89984e026d43565b610bed9355a
evamy <antriksh.don@gmail.com>
2018-04-11 18:17:59 -0500

    File System Initialization using project 2

---
+++ FileSystem/header/Info/Meta/MetaInfo.h
@@ -0,0 +1,20 @@
+// MetaInfo.hpp
+/*
+    @author: antriksh
+    Version 0: 3/15/2018
+*/
+
+#include "utils.h"
+
+// Store Meta-Data for easy access
+class MetaInfo {
+   public:
+    string name;
+    string chunkName;
+    string server;
+
+    MetaInfo(string name, string chunkName, string server)
+        : name(name), chunkName(chunkName), server(server) {}
+
+    string getChunkFile() { return name + "_" + chunkName; }
+};
